fa9ac1604e0c956902aaf305d52903cd87fc5641
Jeremie Cook <hello@jeremiecook.com>
2021-03-30 09:31:26 +0200

    custom transition

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition name="fade">
+    <router-view />
+  </transition>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: "description",
+          name: "description",
+          content: this.$static.metadata.siteDescription,
+        },
+      ],
+    };
+  },
+};
+</script>
